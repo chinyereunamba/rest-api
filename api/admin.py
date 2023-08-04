5c67f5c19917b7e50a0d2aeb9908947e6d8810bb
Chinyere <chinyereunamba17@gmail.com>
2023-08-04 13:15:56 +0100

    create note model

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Note, Tag
+
 # Register your models here.
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created', 'updated']
+    readonly_fields = ['created', 'updated']
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['tag', 'date_created']
+
+admin.site.register(Note, NoteAdmin)
+admin.site.register(Tag, TagAdmin)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=100, unique=True)
+    date_created = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.tag
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField(blank=True, null=True)
+    note = models.TextField()
+    created = models.DateTimeField(auto_now_add=True, blank=True)
+    updated = models.DateTimeField(auto_now=True, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self) -> str:
+        return self.title
